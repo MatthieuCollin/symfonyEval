b08009278e5472bd00054f5a1d28c5f788dadb3d
MatthieuCollin <101406670+MatthieuCollin@users.noreply.github.com>
2023-05-19 15:27:29 +0200

    Finalisation du site

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Node\Expr\Cast\Array_;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function viewHomepage(): Response
+    public function viewHomepage(EntityManagerInterface $entityManager): Response
     {
+
+        $repoProduits = $entityManager->getRepository(Produit::class);
+        $produits = $repoProduits->findAll();
+
+        $size = count($produits);
+
+        $i = $size - 5;
+
+        $data = [];
+
+        while($i < $size){
+
+            $data[] = [
+                'id' => $produits[$i]->getId(),
+                'nom' => $produits[$i]->getNom(),
+                'img' => $produits[$i]->getImage()
+            ];
+
+            $i++;
+        };
+
+
         return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+            'data' => $data,
         ]);
     }
 }
